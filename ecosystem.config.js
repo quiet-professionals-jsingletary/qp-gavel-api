1f050da12cf0085c4fb28521a6ad6f69516cb147
James Singletary <jsingletary@quietprofessionalsllc.com>
2021-06-29 13:49:11 -0400

    CI/CD Pipeline and config updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-  apps:[
-    {
-      script: "npx serve -s"
-    }
-  ]
-};
+// module.exports = {
+//   apps:[
+//     {
+//       script: "npx serve -s"
+//     }
+//   ]
+// };
