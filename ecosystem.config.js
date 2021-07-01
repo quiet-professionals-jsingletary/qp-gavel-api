625225043ee2ea61b3413a25995e6ce671dd47bd
James Singletary <jsingletaryiv@gmail.com>
2021-07-01 08:12:05 -0400

    CI/CD Updates for API deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,15 @@ module.exports = {
   apps: [{
     name: "gavel-api",
     script: "./server.js",
-    exec_mode : "cluster",
-    instances : "4",
+    exec_mode: "cluster",
+    instances: 2,
     watch: true,
+    ignore_watch: ["[\/\\]\./", "node_modules"],
     env: {
-      NODE_ENV: 'development'
+      NODE_ENV: "development"
     },
     env_production: {
-      NODE_ENV: 'production'
+      NODE_ENV: "production"
     }
   }]
 }
