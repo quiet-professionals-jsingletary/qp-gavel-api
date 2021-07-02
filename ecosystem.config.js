51120f925edb78972d6ccbbe9d9b1356dda8bfbd
James Singletary <jsingletary@quietprofessionalsllc.com>
2021-07-01 20:19:29 -0400

    CI/CD Pipeline Updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,11 @@ module.exports = {
   apps: [{
     name: "gavel-api",
     script: "./server.js",
-    exec_mode: "cluster",
-    instances: 2,
+    instances: 1,
+    exec_mode: "fork",
     watch: true,
+    autorestart: true,
+    ignore_watch: ["node_modules"],
     env_development: {
       NODE_ENV: "development"
     },
